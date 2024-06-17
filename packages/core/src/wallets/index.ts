d262fa930fff6c3c60d745a8c0cd7f2d88a61163
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-06-17 16:47:08 +1200

    chore: export deep link wallet

--- packages/core/src/wallets/index.ts
+++ packages/core/src/wallets/index.ts
@@ -1,4 +1,5 @@
 export * from "./aggregator/index.js";
+export { default as DeepLinkWallet } from "./deep-link.js";
 export { initializeWallets } from "./initialize-wallets.js";
 export { default as InjectedWallet } from "./injected.js";
 export { default as WalletConnect } from "./wallet-connect/index.js";
