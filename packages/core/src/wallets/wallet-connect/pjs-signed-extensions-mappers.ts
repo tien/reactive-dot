a7586c0c794a80ccfcda813189b8ce041479509e
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-06-18 13:17:41 +1200

    chore: format unformatted file

--- packages/core/src/wallets/wallet-connect/pjs-signed-extensions-mappers.ts
+++ packages/core/src/wallets/wallet-connect/pjs-signed-extensions-mappers.ts
@@ -1,4 +1,4 @@
-import { HexString } from "@polkadot-api/substrate-bindings"
+import { HexString } from "@polkadot-api/substrate-bindings";
 import {
   Bytes,
   Struct,
@@ -6,61 +6,61 @@ import {
   u32,
   Option,
   compactBn,
-} from "@polkadot-api/substrate-bindings"
-import { toHex } from "@polkadot-api/utils"
+} from "@polkadot-api/substrate-bindings";
+import { toHex } from "@polkadot-api/utils";
 
 type SignedExtension = {
-  value: Uint8Array
-  additionalSigned: Uint8Array
-}
+  value: Uint8Array;
+  additionalSigned: Uint8Array;
+};
 
 const toPjsHex = (value: number | bigint, minByteLen?: number) => {
-  let inner = value.toString(16)
-  inner = (inner.length % 2 ? "0" : "") + inner
-  const nPaddedBytes = Math.max(0, (minByteLen || 0) - inner.length / 2)
-  return "0x" + "00".repeat(nPaddedBytes) + inner
-}
+  let inner = value.toString(16);
+  inner = (inner.length % 2 ? "0" : "") + inner;
+  const nPaddedBytes = Math.max(0, (minByteLen || 0) - inner.length / 2);
+  return "0x" + "00".repeat(nPaddedBytes) + inner;
+};
 
 export const CheckGenesis = ({
   additionalSigned,
 }: SignedExtension): { genesisHash: string } => ({
   genesisHash: toHex(additionalSigned),
-})
+});
 
 export const CheckNonce = ({
   value,
 }: SignedExtension): { nonce: HexString } => {
   // nonce is a u32 in pjs => 4 bytes
-  return { nonce: toPjsHex(compact.dec(value), 4) }
-}
+  return { nonce: toPjsHex(compact.dec(value), 4) };
+};
 
 export const CheckTxVersion = ({
   additionalSigned,
 }: SignedExtension): { transactionVersion: HexString } => {
-  return { transactionVersion: toPjsHex(u32.dec(additionalSigned), 4) }
-}
+  return { transactionVersion: toPjsHex(u32.dec(additionalSigned), 4) };
+};
 
 const assetTxPaymentDec = Struct({
   tip: compact,
   asset: Option(Bytes(Infinity)),
-}).dec
+}).dec;
 
 export const ChargeAssetTxPayment = ({
   value,
 }: SignedExtension): { aseetId?: string; tip?: string } => {
-  const { tip, asset } = assetTxPaymentDec(value)
+  const { tip, asset } = assetTxPaymentDec(value);
 
   return {
     ...(asset ? { assetId: toHex(asset) } : {}),
     tip: toPjsHex(tip, 16),
-  }
-}
+  };
+};
 
 export const ChargeTransactionPayment = ({
   value,
 }: SignedExtension): { tip: HexString } => ({
   tip: toPjsHex(compactBn.dec(value), 16), // u128 => 16 bytes
-})
+});
 
 export const CheckMortality = (
   { value, additionalSigned }: SignedExtension,
@@ -69,13 +69,13 @@ export const CheckMortality = (
   era: toHex(value),
   blockHash: toHex(additionalSigned),
   blockNumber: toPjsHex(blockNumber, 4),
-})
+});
 
 export const CheckSpecVersion = ({
   additionalSigned,
 }: SignedExtension): { specVersion: HexString } => ({
   specVersion: toPjsHex(u32.dec(additionalSigned), 4),
-})
+});
 
 // we create the tx without metadata hash, it's optional for PJS
-export const CheckMetadataHash = () => ({})
+export const CheckMetadataHash = () => ({});
