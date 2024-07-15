95ff6a454632cce3974b70080cc3933038e294f8
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-07-15 20:10:18 +1200

    refactor: extract wallets and accounts fetching logic to core

---
+++ packages/core/src/actions/getConnectedWallets.ts
@@ -0,0 +1,22 @@
+import type { MaybeAsync } from "../types.js";
+import { toObservable } from "../utils.js";
+import type Wallet from "../wallets/wallet.js";
+import { combineLatest } from "rxjs";
+import { map, switchMap } from "rxjs/operators";
+
+export function getConnectedWallets(wallets: MaybeAsync<Wallet[]>) {
+  return toObservable(wallets).pipe(
+    switchMap((wallets) =>
+      combineLatest(
+        wallets.map((wallet) =>
+          wallet.connected$.pipe(
+            map((connected) => [wallet, connected] as const),
+          ),
+        ),
+      ),
+    ),
+    map((wallets) =>
+      wallets.filter(([_, connected]) => connected).map(([wallet]) => wallet),
+    ),
+  );
+}
--- packages/react/src/stores/accounts.ts
+++ packages/react/src/stores/accounts.ts
@@ -1,12 +1,10 @@
 import { withAtomFamilyErrorCatcher } from "../utils/jotai.js";
 import { chainSpecDataAtomFamily } from "./client.js";
 import { walletsAtom } from "./wallets.js";
-import type { ChainId } from "@reactive-dot/core";
+import { getAccounts, type ChainId } from "@reactive-dot/core";
 import type { Atom } from "jotai";
 import { atomFamily, atomWithObservable } from "jotai/utils";
 import type { InjectedPolkadotAccount } from "polkadot-api/pjs-signer";
-import { combineLatest, from } from "rxjs";
-import { map, switchMap } from "rxjs/operators";
 
 export const accountsAtom = atomFamily(
   (
@@ -17,21 +15,6 @@ export const accountsAtom = atomFamily(
       chainId,
       atomWithObservable,
     )((get) =>
-      from(
-        Promise.all([get(chainSpecDataAtomFamily(chainId)), get(walletsAtom)]),
-      ).pipe(
-        switchMap(([chainSpec, wallets]) =>
-          combineLatest(wallets.map((wallet) => wallet.accounts$)).pipe(
-            map((wallets) => wallets.flat()),
-            map((accounts) =>
-              accounts.filter(
-                (account) =>
-                  !account.genesisHash ||
-                  chainSpec.genesisHash.includes(account.genesisHash),
-              ),
-            ),
-          ),
-        ),
-      ),
+      getAccounts(get(walletsAtom), get(chainSpecDataAtomFamily(chainId))),
     ),
 );
