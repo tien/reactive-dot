ceb8723ac3babcb2db4172a934f5bbeb3cd6f798
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-08-15 18:43:02 +1200

    perf: implement `weakAtomFamily` (#117)

--- packages/react/src/stores/block.ts
+++ packages/react/src/stores/block.ts
@@ -1,10 +1,11 @@
+import { weakAtomFamily } from "../utils/jotai.js";
 import { clientAtomFamily } from "./client.js";
 import { type ChainId, getBlock } from "@reactive-dot/core";
-import { atomFamily, atomWithObservable } from "jotai/utils";
+import { atomWithObservable } from "jotai/utils";
 import { from } from "rxjs";
 import { switchMap } from "rxjs/operators";
 
-export const finalizedBlockAtomFamily = atomFamily((chainId: ChainId) =>
+export const finalizedBlockAtomFamily = weakAtomFamily((chainId: ChainId) =>
   atomWithObservable((get) =>
     from(get(clientAtomFamily(chainId))).pipe(
       switchMap((client) => getBlock(client, { tag: "finalized" })),
@@ -12,7 +13,7 @@ export const finalizedBlockAtomFamily = atomFamily((chainId: ChainId) =>
   ),
 );
 
-export const bestBlockAtomFamily = atomFamily((chainId: ChainId) =>
+export const bestBlockAtomFamily = weakAtomFamily((chainId: ChainId) =>
   atomWithObservable((get) =>
     from(get(clientAtomFamily(chainId))).pipe(
       switchMap((client) => getBlock(client, { tag: "best" })),
