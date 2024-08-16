c35d5a696b6768da6b04dddb764ce7535ace4fca
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-08-16 23:30:34 +1200

    refactor: simplify global states
    Hydrate only a single config atom, from which other global states will be derived.

--- packages/react/src/stores/wallets.ts
+++ packages/react/src/stores/wallets.ts
@@ -1,15 +1,24 @@
+import { configAtom } from "./config.js";
 import { aggregateWallets, getConnectedWallets } from "@reactive-dot/core";
-import type { Wallet, WalletAggregator } from "@reactive-dot/core/wallets.js";
+import { Wallet, WalletAggregator } from "@reactive-dot/core/wallets.js";
 import { atom } from "jotai";
 import { atomWithObservable } from "jotai/utils";
 
-export const aggregatorsAtom = atom<WalletAggregator[]>([]);
-
-const aggregatorWallets = atomWithObservable((get) =>
-  aggregateWallets(get(aggregatorsAtom)),
+export const walletAggregatorsAtom = atom(
+  (get) =>
+    get(configAtom).wallets?.filter(
+      (aggregator) => aggregator instanceof WalletAggregator,
+    ) ?? [],
 );
 
-export const directWalletsAtom = atom<Wallet[]>([]);
+const aggregatorWallets = atomWithObservable((get) =>
+  aggregateWallets(get(walletAggregatorsAtom)),
+);
+
+export const directWalletsAtom = atom<Wallet[]>(
+  (get) =>
+    get(configAtom).wallets?.filter((wallet) => wallet instanceof Wallet) ?? [],
+);
 
 export const walletsAtom = atom(async (get) => [
   ...get(directWalletsAtom),
