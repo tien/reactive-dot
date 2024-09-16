08a464c2f78423ae65eaab350c6cdabb9e040e7d
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-09-16 14:33:14 +1200

    refactor: remove unnecessary barrel exports & `export *` (#173)

--- packages/core/src/actions/index.ts
+++
@@ -1,8 +0,0 @@
-export { aggregateWallets } from "./aggregate-wallets.js";
-export { connectWallet } from "./connect-wallet.js";
-export { disconnectWallet } from "./disconnect-wallet.js";
-export { getAccounts } from "./get-accounts.js";
-export { getBlock, unstable_getBlockExtrinsics } from "./get-block.js";
-export { getClient } from "./get-client.js";
-export { getConnectedWallets } from "./get-connected-wallets.js";
-export { preflight, query } from "./query.js";
--- packages/core/src/wallets/index.ts
+++ packages/core/src/wallets/index.ts
@@ -1,5 +1,6 @@
 export { WalletAggregator } from "./aggregator.js";
 export { DeepLinkWallet } from "./deep-link-wallet.js";
 export { initializeWallets } from "./initialize-wallets.js";
-export { InjectedWallet, InjectedWalletAggregator } from "./injected/index.js";
+export { InjectedWalletAggregator } from "./injected/aggregator.js";
+export { InjectedWallet } from "./injected/wallet.js";
 export { Wallet } from "./wallet.js";
