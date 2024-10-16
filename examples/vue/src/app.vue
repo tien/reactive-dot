6b842986a5d9ba1b58c298205fb0efdadff8ddd1
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-10-16 19:19:43 +1300

    chore: add chain switcher to example

--- examples/vue/src/app.vue
+++ examples/vue/src/app.vue
@@ -2,18 +2,33 @@
 import Mutation from "./mutation.vue";
 import Query from "./query.vue";
 import WalletConnection from "./wallet-connection.vue";
-import { provideChain } from "@reactive-dot/vue";
+import type { ChainId } from "@reactive-dot/core";
+import { provideChain, useChainIds } from "@reactive-dot/vue";
+import { ref } from "vue";
 
-provideChain("polkadot");
+const chainIds = useChainIds();
+const selectedChainId = ref<ChainId>("polkadot");
+
+provideChain(selectedChainId);
 </script>
 
 <template>
-  <Suspense>
+  <div>
+    <label>
+      Chain
+      <select v-model="selectedChainId">
+        <option v-for="chainId in chainIds" :value="chainId">
+          {{ chainId }}
+        </option>
+      </select>
+    </label>
+  </div>
+  <Suspense :key="selectedChainId">
     <div>
       <WalletConnection />
       <Query />
       <Mutation />
     </div>
-    <template #fallback> Loading... </template>
+    <template #fallback>Loading... </template>
   </Suspense>
 </template>
