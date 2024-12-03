aeef0303347668d7c53de3373f581b95a723fb17
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-12-04 09:29:23 +1300

    fix: pjs-extension wallet detection (#382)

---
+++ .changeset/ninety-snails-type.md
@@ -0,0 +1,5 @@
+---
+"@reactive-dot/core": patch
+---
+
+Fixed PJS wallet detection, this is a workaround for the following [issue](https://github.com/polkadot-js/extension/issues/1475).
--- packages/core/src/wallets/injected/provider.ts
+++ packages/core/src/wallets/injected/provider.ts
@@ -2,12 +2,17 @@ import { WalletProvider } from "../provider.js";
 import { InjectedWallet, type InjectedWalletOptions } from "./wallet.js";
 import { getInjectedExtensions } from "polkadot-api/pjs-signer";
 
+// TODO: remove once https://github.com/polkadot-js/extension/issues/1475 is fixed
+const artificialDelay = new Promise((resolve) => setTimeout(resolve, 500));
+
 export class InjectedWalletProvider extends WalletProvider {
   constructor(private readonly options?: InjectedWalletOptions) {
     super();
   }
 
-  getWallets() {
+  async getWallets() {
+    await artificialDelay;
+
     return getInjectedExtensions().map(
       (name) => new InjectedWallet(name, this.options),
     );
