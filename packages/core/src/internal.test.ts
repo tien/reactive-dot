3c507dd07f58efcb30b7d85d2baa62da7d6f8c63
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-02-11 14:06:43 +1300

    test: exports (#472)

---
+++ packages/core/src/internal.test.ts
@@ -0,0 +1,11 @@
+import * as exports from "./internal.js";
+import { expect, it } from "vitest";
+
+it("should match inline snapshot", () =>
+  expect(Object.keys(exports)).toMatchInlineSnapshot(`
+    [
+      "flatHead",
+      "toObservable",
+      "stringify",
+    ]
+  `));
