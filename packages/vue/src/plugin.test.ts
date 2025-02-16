5c8560610f9d33ce42739fc60f3648ef6569b751
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-02-17 12:43:45 +1300

    test(vue): plugin (#498)

---
+++ packages/vue/src/plugin.test.ts
@@ -0,0 +1,50 @@
+import * as walletsInitializer from "./composables/use-wallets-initializer";
+import { configKey, lazyValuesKey, mutationEventKey } from "./keys.js";
+import { ReactiveDotPlugin } from "./plugin.js";
+import type { Config } from "@reactive-dot/core";
+import { beforeEach, expect, it, vi } from "vitest";
+import type { App } from "vue";
+
+const mockApp = {
+  provide: vi.fn(),
+  runWithContext: vi.fn((fn) => fn()),
+} as unknown as App;
+
+const mockExecute = vi.fn();
+
+vi.spyOn(walletsInitializer, "useWalletsInitializer").mockReturnValue({
+  execute: mockExecute,
+} as unknown as ReturnType<typeof walletsInitializer.useWalletsInitializer>);
+
+const mockConfig = {} as unknown as Config;
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
+
+it("should provide config using configKey", () => {
+  ReactiveDotPlugin.install(mockApp, mockConfig);
+
+  expect(mockApp.provide).toHaveBeenCalledWith(configKey, mockConfig);
+});
+
+it("should provide empty Map using lazyValuesKey", () => {
+  ReactiveDotPlugin.install(mockApp, mockConfig);
+
+  expect(mockApp.provide).toHaveBeenCalledWith(lazyValuesKey, expect.any(Map));
+});
+
+it("should provide shallowRef using mutationEventKey", () => {
+  ReactiveDotPlugin.install(mockApp, mockConfig);
+
+  expect(mockApp.provide).toHaveBeenCalledWith(
+    mutationEventKey,
+    expect.any(Object),
+  );
+});
+
+it("should initialize wallets", () => {
+  ReactiveDotPlugin.install(mockApp, mockConfig);
+
+  expect(mockExecute).toHaveBeenCalled();
+});
