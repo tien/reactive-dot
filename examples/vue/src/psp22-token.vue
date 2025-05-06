5e6727a2f0ae4320f23b600e56ae1a93a1d9d73f
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-05-07 11:09:00 +1200

    chore: add vue ink example

---
+++ examples/vue/src/psp22-token.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { psp22 } from "./contracts";
+import { useQuery } from "@reactive-dot/vue";
+import { defineProps, toRefs } from "vue";
+
+const { address } = defineProps<{
+  address: string;
+}>();
+
+const { data } = await useQuery(
+  (builder) =>
+    builder
+      .storage("Timestamp", "Now")
+      .contract(psp22, address, (builder) =>
+        builder
+          .message("PSP22Metadata::token_name")
+          .message("PSP22Metadata::token_decimals")
+          .message("PSP22Metadata::token_symbol")
+          .message("PSP22::total_supply"),
+      ),
+  { chainId: "pop_testnet" },
+);
+
+const [timestamp, psp22Data] = toRefs(data.value);
+
+const [tokenName, tokenDecimals, tokenSymbol, totalSupply] = toRefs(
+  psp22Data.value,
+);
+</script>
+
+<template>
+  <dl>
+    <dt>Timestamp</dt>
+    <dd>{{ new Date(Number(timestamp)).toLocaleString() }}</dd>
+
+    <dt>Token name</dt>
+    <dd>{{ tokenName ?? "N/A" }}</dd>
+
+    <dt>Token symbol</dt>
+    <dd>{{ tokenSymbol }}</dd>
+
+    <dt>Token decimals</dt>
+    <dd>{{ tokenDecimals }}</dd>
+
+    <dt>Total supply</dt>
+    <dd>{{ totalSupply.toLocaleString() }}</dd>
+  </dl>
+</template>
