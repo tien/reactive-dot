db1b009ee2ef1d8ba281c8caaa5440becaefe628
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-05-09 22:39:02 +1200

    chore: add vue contract example

--- examples/vue/src/contracts-section.vue
+++ examples/vue/src/contracts-section.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import FlipperContract from "./flipper-contract.vue";
 import Psp22Token from "./psp22-token.vue";
+import { provideChain } from "@reactive-dot/vue";
+
+provideChain("pop_testnet");
 </script>
 
 <template>
   <section>
     <h2>Contracts</h2>
     <Psp22Token address="16dg7W38UBuVZQMZkTVYmYPLuaFQ3bfE9ZLS4jAeKJzC2E7d" />
+    <FlipperContract
+      address="14MRHYDcgWmeEsJbfjvjrnyeDrTaF1pAK4UkQEbAyDvP2UqF"
+    />
   </section>
 </template>
--- examples/vue/src/contracts.ts
+++ examples/vue/src/contracts.ts
@@ -2,3 +2,7 @@ import { contracts } from "@polkadot-api/descriptors";
 import { defineContract } from "@reactive-dot/core";
 
 export const psp22 = defineContract({ descriptor: contracts.psp22 });
+
+export const flipper = defineContract({
+  descriptor: contracts.flipper,
+});
