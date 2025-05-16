2ac3d36e32e48d201e7fb9fd2df58c8b31715059
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-05-16 12:03:22 +1200

    fix(react): suspense flickers

---
+++ .changeset/blue-kids-wonder.md
@@ -0,0 +1,5 @@
+---
+"@reactive-dot/react": patch
+---
+
+Fixed intermittent suspense flickers.
--- packages/react/src/hooks/use-typed-api.ts
+++ packages/react/src/hooks/use-typed-api.ts
@@ -1,11 +1,12 @@
 import { atomFamilyWithErrorCatcher } from "../utils/jotai/atom-family-with-error-catcher.js";
 import type { ChainHookOptions } from "./types.js";
+import { useAtomValue } from "./use-atom-value.js";
 import { internal_useChainId } from "./use-chain-id.js";
 import { clientAtom } from "./use-client.js";
 import { useConfig } from "./use-config.js";
 import { BaseError, type ChainId, type Config } from "@reactive-dot/core";
 import type { ChainDescriptorOf } from "@reactive-dot/core/internal.js";
-import { atom, useAtomValue } from "jotai";
+import { atom } from "jotai";
 import { soon } from "jotai-derive";
 import type { TypedApi } from "polkadot-api";
 
