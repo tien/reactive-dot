6174ce9ccda99f01a50fc4aae6df285469da2b08
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-05-21 01:30:17 +1200

    perf(core): remove artificial delay in `getWallets` method

---
+++ .changeset/ripe-foxes-sell.md
@@ -0,0 +1,5 @@
+---
+"@reactive-dot/core": patch
+---
+
+Removed artificial delay previously implemented as a workaround for a [Polkadot.js extension bug](https://github.com/polkadot-js/extension/issues/1475).
--- packages/core/src/wallets/injected/provider.ts
+++ packages/core/src/wallets/injected/provider.ts
@@ -1,17 +1,12 @@
 import { WalletProvider } from "../provider.js";
 import { InjectedWallet, type InjectedWalletOptions } from "./wallet.js";
 
-// TODO: remove once https://github.com/polkadot-js/extension/issues/1475 is fixed
-const artificialDelay = new Promise((resolve) => setTimeout(resolve, 500));
-
 export class InjectedWalletProvider extends WalletProvider {
   constructor(private readonly options?: InjectedWalletOptions) {
     super();
   }
 
   async getWallets() {
-    await artificialDelay;
-
     const { getInjectedExtensions } = await import("polkadot-api/pjs-signer");
 
     return getInjectedExtensions().map(
