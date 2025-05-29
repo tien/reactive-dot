a333848e4a115d5f32a6d141c9b43d1c7a87518e
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-05-29 15:04:28 +1200

    refactor(react): extract `tapTx`

--- packages/react/src/hooks/use-mutation.ts
+++ packages/react/src/hooks/use-mutation.ts
@@ -1,35 +1,22 @@
-import {
-  type MutationEvent,
-  MutationEventSubjectContext,
-} from "../contexts/mutation.js";
+import { MutationEventSubjectContext } from "../contexts/mutation.js";
 import { SignerContext } from "../contexts/signer.js";
+import { tapTx } from "../utils/tap-tx.js";
 import type { ChainHookOptions } from "./types.js";
 import { useAsyncAction } from "./use-async-action.js";
 import { internal_useChainId } from "./use-chain-id.js";
 import { useConfig } from "./use-config.js";
 import { typedApiAtom } from "./use-typed-api.js";
 import type { ChainId } from "@reactive-dot/core";
-import { MutationError, pending } from "@reactive-dot/core";
+import { MutationError } from "@reactive-dot/core";
 import type {
   ChainDescriptorOf,
-  GenericTransaction,
   TxOptionsOf,
 } from "@reactive-dot/core/internal.js";
 import { useAtomCallback } from "jotai/utils";
-import type {
-  PolkadotSigner,
-  Transaction,
-  TxEvent,
-  TypedApi,
-} from "polkadot-api";
+import type { PolkadotSigner, Transaction, TypedApi } from "polkadot-api";
 import { use, useCallback } from "react";
-import {
-  from,
-  type Observable,
-  type Subject,
-  type MonoTypeOperatorFunction,
-} from "rxjs";
-import { catchError, switchMap, tap } from "rxjs/operators";
+import { from } from "rxjs";
+import { switchMap } from "rxjs/operators";
 
 /**
  * Hook for sending transactions to chains.
@@ -106,34 +93,4 @@ export function useMutation<
       ),
     ),
   );
-}
-
-/**
- * @internal
- */
-export function tapTx<T extends TxEvent>(
-  mutationEventSubject: Subject<MutationEvent>,
-  chainId: ChainId,
-  transaction: GenericTransaction,
-): MonoTypeOperatorFunction<T> {
-  return (source: Observable<T>) => {
-    const eventProps = {
-      id: globalThis.crypto.randomUUID(),
-      chainId,
-      call: transaction.decodedCall,
-    };
-
-    mutationEventSubject.next({ ...eventProps, value: pending });
-
-    return source.pipe(
-      tap((value) => mutationEventSubject.next({ ...eventProps, value })),
-      catchError((error) => {
-        mutationEventSubject.next({
-          ...eventProps,
-          value: MutationError.from(error),
-        });
-        throw error;
-      }),
-    );
-  };
 }
