6f1a61bb9f2161078663458e35a49b73c38d5823
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-06-12 20:03:11 +1200

    test(react): contract queries (#720)

--- packages/react/src/hooks/use-query.test.tsx
+++ packages/react/src/hooks/use-query.test.tsx
@@ -1,7 +1,8 @@
 import { ChainProvider } from "../contexts/chain.js";
 import { ReactiveDotProvider } from "../contexts/provider.js";
 import { useLazyLoadQuery } from "./use-query.js";
-import { defineConfig } from "@reactive-dot/core";
+import { defineConfig, defineContract } from "@reactive-dot/core";
+import type { SimpleInkQueryInstruction } from "@reactive-dot/core/internal.js";
 import { renderHook } from "@testing-library/react";
 import { atom } from "jotai";
 import { act, Suspense } from "react";
@@ -34,6 +35,35 @@ vi.mock("./use-typed-api.js", () => ({
   ),
 }));
 
+vi.mock("./use-ink-client.js", () => ({
+  inkClientAtom: vi.fn(() => atom({})),
+}));
+
+vi.mock(
+  "@reactive-dot/core/internal/actions.js",
+  async (getOriginalModule) => ({
+    ...(await getOriginalModule()),
+    queryInk: vi.fn(
+      async (
+        _,
+        __,
+        address: string,
+        instruction: SimpleInkQueryInstruction,
+      ) => [
+        `contract-${address}`,
+        Object.fromEntries(
+          Object.entries(instruction).filter(
+            ([_, value]) => value !== undefined,
+          ),
+        ),
+      ],
+    ),
+  }),
+);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const testContract = defineContract({ descriptor: {} as any });
+
 describe("useLazyLoadQuery", () => {
   it("fetches single queries", async () => {
     const {
@@ -59,6 +89,38 @@ describe("useLazyLoadQuery", () => {
     expect(current).toMatchInlineSnapshot(`"test-value"`);
   });
 
+  it("fetches single contract queries", async () => {
+    const {
+      result: { current },
+    } = await act(() =>
+      renderHook(
+        () =>
+          useLazyLoadQuery((query) =>
+            query.contract(testContract, "0x", (query) => query.rootStorage()),
+          ),
+        {
+          wrapper: ({ children }) => (
+            <ReactiveDotProvider config={defineConfig({ chains: {} })}>
+              <ChainProvider chainId="test-chain">
+                <Suspense>{children}</Suspense>
+              </ChainProvider>
+            </ReactiveDotProvider>
+          ),
+        },
+      ),
+    );
+
+    expect(current).toMatchInlineSnapshot(`
+      [
+        "contract-0x",
+        {
+          "instruction": "read-storage",
+          "path": "",
+        },
+      ]
+    `);
+  });
+
   it("fetches multi queries", async () => {
     const {
       result: { current },
@@ -71,7 +133,29 @@ describe("useLazyLoadQuery", () => {
               .storage("test_pallet", "test_storage", ["key"])
               .storages("test_pallet", "test_storage", [["key1"], ["key2"]])
               .runtimeApi("test_pallet", "test_api", ["key"])
-              .runtimeApis("test_pallet", "test_api", [["key1"], ["key2"]]),
+              .runtimeApis("test_pallet", "test_api", [["key1"], ["key2"]])
+              .contract(testContract, "0x", (query) =>
+                query
+                  .rootStorage()
+                  .storage("test_storage", "test_key")
+                  .storages("test_storage", ["test_key1", "test_key2"])
+                  .message("test_message", { data: "test-data" })
+                  .messages("test_message", [
+                    { data: "test-data1" },
+                    { data: "test-data2" },
+                  ]),
+              )
+              .contracts(testContract, ["0x", "0x1"], (query) =>
+                query
+                  .rootStorage()
+                  .storage("test_storage", "test_key")
+                  .storages("test_storage", ["test_key1", "test_key2"])
+                  .message("test_message", { data: "test-data" })
+                  .messages("test_message", [
+                    { data: "test-data1" },
+                    { data: "test-data2" },
+                  ]),
+              ),
           ),
         {
           wrapper: ({ children }) => (
@@ -97,6 +181,209 @@ describe("useLazyLoadQuery", () => {
         [
           "api-value-key1",
           "api-value-key2",
+        ],
+        [
+          [
+            "contract-0x",
+            {
+              "instruction": "read-storage",
+              "path": "",
+            },
+          ],
+          [
+            "contract-0x",
+            {
+              "instruction": "read-storage",
+              "key": "test_key",
+              "path": "test_storage",
+            },
+          ],
+          [
+            [
+              "contract-0x",
+              {
+                "instruction": "read-storage",
+                "key": "test_key1",
+                "path": "test_storage",
+              },
+            ],
+            [
+              "contract-0x",
+              {
+                "instruction": "read-storage",
+                "key": "test_key2",
+                "path": "test_storage",
+              },
+            ],
+          ],
+          [
+            "contract-0x",
+            {
+              "body": {
+                "data": "test-data",
+              },
+              "instruction": "send-message",
+              "name": "test_message",
+            },
+          ],
+          [
+            [
+              "contract-0x",
+              {
+                "body": {
+                  "data": "test-data1",
+                },
+                "instruction": "send-message",
+                "name": "test_message",
+              },
+            ],
+            [
+              "contract-0x",
+              {
+                "body": {
+                  "data": "test-data2",
+                },
+                "instruction": "send-message",
+                "name": "test_message",
+              },
+            ],
+          ],
+        ],
+        [
+          [
+            [
+              "contract-0x",
+              {
+                "instruction": "read-storage",
+                "path": "",
+              },
+            ],
+            [
+              "contract-0x",
+              {
+                "instruction": "read-storage",
+                "key": "test_key",
+                "path": "test_storage",
+              },
+            ],
+            [
+              [
+                "contract-0x",
+                {
+                  "instruction": "read-storage",
+                  "key": "test_key1",
+                  "path": "test_storage",
+                },
+              ],
+              [
+                "contract-0x",
+                {
+                  "instruction": "read-storage",
+                  "key": "test_key2",
+                  "path": "test_storage",
+                },
+              ],
+            ],
+            [
+              "contract-0x",
+              {
+                "body": {
+                  "data": "test-data",
+                },
+                "instruction": "send-message",
+                "name": "test_message",
+              },
+            ],
+            [
+              [
+                "contract-0x",
+                {
+                  "body": {
+                    "data": "test-data1",
+                  },
+                  "instruction": "send-message",
+                  "name": "test_message",
+                },
+              ],
+              [
+                "contract-0x",
+                {
+                  "body": {
+                    "data": "test-data2",
+                  },
+                  "instruction": "send-message",
+                  "name": "test_message",
+                },
+              ],
+            ],
+          ],
+          [
+            [
+              "contract-0x1",
+              {
+                "instruction": "read-storage",
+                "path": "",
+              },
+            ],
+            [
+              "contract-0x1",
+              {
+                "instruction": "read-storage",
+                "key": "test_key",
+                "path": "test_storage",
+              },
+            ],
+            [
+              [
+                "contract-0x1",
+                {
+                  "instruction": "read-storage",
+                  "key": "test_key1",
+                  "path": "test_storage",
+                },
+              ],
+              [
+                "contract-0x1",
+                {
+                  "instruction": "read-storage",
+                  "key": "test_key2",
+                  "path": "test_storage",
+                },
+              ],
+            ],
+            [
+              "contract-0x1",
+              {
+                "body": {
+                  "data": "test-data",
+                },
+                "instruction": "send-message",
+                "name": "test_message",
+              },
+            ],
+            [
+              [
+                "contract-0x1",
+                {
+                  "body": {
+                    "data": "test-data1",
+                  },
+                  "instruction": "send-message",
+                  "name": "test_message",
+                },
+              ],
+              [
+                "contract-0x1",
+                {
+                  "body": {
+                    "data": "test-data2",
+                  },
+                  "instruction": "send-message",
+                  "name": "test_message",
+                },
+              ],
+            ],
+          ],
         ],
       ]
     `);
